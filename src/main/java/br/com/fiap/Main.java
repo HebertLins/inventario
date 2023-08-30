1da141713381364c5de6dce236e59406c976beda
HebertLins <hebertclins@gmail.com>
2023-08-30 20:41:53 -0300

    Pronto

--- src/main/java/br/com/fiap/Main.java
+++ src/main/java/br/com/fiap/Main.java
@@ -1,13 +1,31 @@
 package br.com.fiap;
 
+import br.com.fiap.domain.entity.Bem;
+import br.com.fiap.domain.entity.Departamento;
+import br.com.fiap.domain.entity.Inventario;
+import br.com.fiap.domain.entity.TipoDeBem;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
 
+import javax.swing.*;
+import java.time.LocalDate;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
+
+        EntityManagerFactory factory = Persistence.createEntityManagerFactory("oracle");
+        EntityManager manager = factory.createEntityManager();
+
+
+        TipoDeBem tipo = new TipoDeBem();
+        tipo.setId(null)
+                .setNome("casinha");
 
 
 
     }
+
+
 }
