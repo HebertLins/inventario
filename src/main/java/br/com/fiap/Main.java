ed149efd5d8d43eaba2b3e9f431e80324a71a4d9
HebertLins <hebertclins@gmail.com>
2023-08-30 20:42:43 -0300

    consultaInventario

--- src/main/java/br/com/fiap/Main.java
+++ src/main/java/br/com/fiap/Main.java
@@ -24,7 +24,38 @@ public class Main {
                 .setNome("casinha");
 
 
+        Departamento dep = new Departamento();
+        dep.setId(null)
+                .setNome("Departamento Abdec");
 
+        Inventario inventario = new Inventario();
+        inventario.setId(null)
+                .setRelatorio("Imovel em bom estado")
+                .setDepartamento(dep)
+                .setInicio(LocalDate.of(1990,10,21));
+
+        Bem bem = new Bem();
+        bem.setId(null)
+                .setNome("Casa 567")
+                .setEtiqueta("TesteEtiqueta2")
+                .setLocalizacao(dep)
+                .setTipo(tipo)
+                .setAquisicao(LocalDate.of(2000,03,10));
+
+        manager.getTransaction().begin();
+        manager.persist(bem);
+        manager.getTransaction().commit();
+
+        Long id = Long.valueOf(JOptionPane.showInputDialog("ID do Inventario:"));
+        Inventario inventario1 = manager.find(Inventario.class, id);
+        System.out.println(inventario1);
+        //ID 52 PARA TESTES
+
+        List<Bem> list = manager.createQuery("FROM Bem").getResultList();
+        list.forEach(System.out::println);
+
+        manager.close();
+        factory.close();
     }
 
 
